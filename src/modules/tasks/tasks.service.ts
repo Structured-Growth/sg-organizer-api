1c248f3ee4a20b12e43a09cd95d89516eba825d0
anton-devline <anton.z@devline.pro>
2024-11-11 17:37:59 +0000

    feat(SG-493): Task model: Multiple task assignments (accounts, groups), metadata field

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,7 +1,38 @@
-import { autoInjectable, inject } from "@structured-growth/microservice-sdk";
+import { autoInjectable, inject, NotFoundError } from "@structured-growth/microservice-sdk";
 import { TasksRepository } from "./tasks.repository";
+import { TaskTypeRepository } from "../task-type/task-type.repository";
+import Task from "../../../database/models/task";
+import { TaskCreateBodyInterface } from "../../interfaces/task-create-body.interface";
+import { TaskUpdateBodyInterface } from "../../interfaces/task-update-body.interface";
 
 @autoInjectable()
 export class TasksService {
-	constructor(@inject("TasksRepository") private taskRepository: TasksRepository) {}
+	constructor(
+		@inject("TasksRepository") private taskRepository: TasksRepository,
+		@inject("TaskTypeRepository") private taskTypeRepository: TaskTypeRepository
+	) {}
+
+	public async create(params: TaskCreateBodyInterface): Promise<Task> {
+		const taskTypeId = await this.taskTypeRepository.read(params.taskTypeId);
+
+		if (!taskTypeId) {
+			throw new NotFoundError(`Task type id ${params.taskTypeId} not found`);
+		}
+
+		return this.taskRepository.create({
+			...params,
+		});
+	}
+
+	public async update(taskId, params: TaskUpdateBodyInterface): Promise<Task> {
+		if (params.taskTypeId) {
+			const taskTypeId = await this.taskTypeRepository.read(params.taskTypeId);
+
+			if (!taskTypeId) {
+				throw new NotFoundError(`Task type id ${params.taskTypeId} not found`);
+			}
+		}
+
+		return this.taskRepository.update(taskId, { ...params });
+	}
 }
